80964e4bbf53e7ca0b3a390711733c0ee3571920
Dhruv Marakana <dhruvmarakana43@gmail.com>
2025-12-29 11:33:23 +0530

    Add files via upload

---
+++ data type.py
@@ -0,0 +1,171 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+x="hellow"
+print(x)
+hellow
+x="20"
+print(x)
+20
+x="10.3"
+print(x)
+10.3
+x=3j
+print(x)
+3j
+
+x="apple","banana","cherry"
+print(x)
+('apple', 'banana', 'cherry')
+x={"apple","banana","cherry"}
+print(x)
+{'cherry', 'banana', 'apple'}
+x=true
+Traceback (most recent call last):
+  File "<pyshell#13>", line 1, in <module>
+    x=true
+NameError: name 'true' is not defined. Did you mean: 'True'?
+x="true"
+print(x)
+true
+a=10
+b=20
+print(a+b)
+30
+a=10
+b=20
+SyntaxError: multiple statements found while compiling a single statement
+print(x)
+true
+a=10
+b=20
+SyntaxError: multiple statements found while compiling a single statement
+a=10
+b=22
+SyntaxError: multiple statements found while compiling a single statement
+print(a-b)
+-10
+print(a*b)
+200
+print(a\b)
+SyntaxError: unexpected character after line continuation character
+print(a/b)
+0.5
+x='a'
+print(x)
+a
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+x="hellow"
+print(x)
+hellow
+x="20"
+print(x)
+20
+x="10.3"
+print(x)
+10.3
+x=3j
+print(x)
+3j
+
+x="apple","banana","cherry"
+print(x)
+('apple', 'banana', 'cherry')
+x={"apple","banana","cherry"}
+print(x)
+{'cherry', 'banana', 'apple'}
+x=true
+Traceback (most recent call last):
+  File "<pyshell#13>", line 1, in <module>
+    x=true
+NameError: name 'true' is not defined. Did you mean: 'True'?
+x="true"
+print(x)
+true
+a=10
+b=20
+print(a+b)
+30
+a=10
+b=20
+SyntaxError: multiple statements found while compiling a single statement
+print(x)
+true
+a=10
+b=20
+SyntaxError: multiple statements found while compiling a single statement
+a=10
+b=22
+SyntaxError: multiple statements found while compiling a single statement
+print(a-b)
+-10
+print(a*b)
+200
+print(a\b)
+SyntaxError: unexpected character after line continuation character
+print(a/b)
+0.5
+x='a'
+print(x)
+S
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+print(a//b)
+0
+print(%)
+SyntaxError: invalid syntax
+print(a%b)
+10
+print(a**b)
+100000000000000000000
+print(a==b)
+False
+print(a!=b)
+True
+print(a>b)
+False
+print(a<b)
+True
+print(a>=b)
+False
+print(a<=b)
+True
+print(a and b)
+20
+print(a or b)
+10
+print(not a)
+False
+#bitwise
+print(a & b)
+0
+print(a | b)
+30
+print(a^b)
+30
+print(a<<b)
+10485760
+print(a>>b)
+0
+print(~a)
+-11
+#membership
+list=[1,2,3,4,5]
+print(3 in list)
+True
+print(ab)
+KeyboardInterrupt
+print(5 in list)
+True
+#identity
+c=[1,2]
+print(a is b)
+False
+print(a is c)
+False
+print(a is not b)
+True
+#string
+s1="hello"
+s2="world"
+print(s1+s2)
+helloworld
