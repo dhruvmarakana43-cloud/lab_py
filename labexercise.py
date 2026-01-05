72e4a544e7a1e1df7b2c4456cdbd765c8f4667ec
Dhruv Marakana <dhruvmarakana43@gmail.com>
2026-01-05 11:21:44 +0530

    Add files via upload

---
+++ labexercise.py
@@ -0,0 +1,48 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+x="hellow world"
+print(x)
+hellow world
+a=10
+print(a)
+10
+x="true"
+y="false"
+print(y)
+false
+x=("apple","banana","cherry")
+print(x)
+('apple', 'banana', 'cherry')
+
+
+
+x=["apple","banana","cherry"]
+print(x)
+['apple', 'banana', 'cherry']
+x={"apple","banana","cherry"}
+print(x)
+{'apple', 'banana', 'cherry'}
+x=1j;
+print(x)
+1j
+x=10.5555555
+print(x)
+10.5555555
+x=?
+SyntaxError: invalid syntax
+print(x)
+10.5555555
+x=
+SyntaxError: invalid syntax
+
+x=bytearry(a)
+Traceback (most recent call last):
+  File "<pyshell#24>", line 1, in <module>
+    x=bytearry(a)
+NameError: name 'bytearry' is not defined. Did you mean: 'bytearray'?
+x=([65,66,67])
+print(x)
+[65, 66, 67]
+x=""
+print(x)
+
