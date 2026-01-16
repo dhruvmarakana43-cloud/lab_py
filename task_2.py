17c3452a362642c5110c5bc686e166b80feaf4ee
Dhruv Marakana <dhruvmarakana43@gmail.com>
2026-01-16 11:40:32 +0530

    Add files via upload

---
+++ task_2.py
@@ -0,0 +1,71 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+for i in range(1, 6):
+      print(i)
+
+      
+1
+2
+3
+4
+5
+my_string = "Hello, Python!"
+length = len(my_string)
+print(length)
+
+14
+a = 10
+b = 20
+maximum = max(a, b)
+print("Maximum is:", maximum)
+Maximum is: 20
+P = 1000
+KeyboardInterrupt
+R = 5
+T = 2
+T = 2SI = (P * R * T) / 100
+SyntaxError: invalid decimal literal
+SI = (P * R * T) / 100
+print("Simple Interest is:", SI)
+Simple Interest is: 100.0
+print("welcom")
+welcom
+my_string = "Python"
+first_char = my_string[0]
+print("The first character is:", first_char)
+The first character is: P
+last_char = my_string[-1]last_char = my_string[-1]
+SyntaxError: invalid syntax
+last_char = my_string[-1]
+print("The last character is:", last_char)
+The last character is: n
+num = float(input("Enter a number: "))
+Enter a number: 2
+if num > 0:
+   print("The number is positive.")
+elif num < 0:
+    print("The number is negative.")
+else:
+    print("The number is zero.")
+
+    
+The number is positive.
+a = 10
+b = 20
+c = 30
+SyntaxError: multiple statements found while compiling a single statement
+KeyboardInterrupt
+a = 10
+b = 20
+KeyboardInterrupt
+c = 30
+sum_of_numbers = a + b + c
+print("The sum is:", sum_of_numbers)
+The sum is: 60
+num1 = float(input("Enter first number: "))
+Enter first number: 22
+num2 = float(input("Enter second number: "))
+Enter second number: 44
+sum_of_numbers = num1 + num2
+print("The sum of the three numbers is:", sum_of_numbers)
+The sum of the three numbers is: 66.0
