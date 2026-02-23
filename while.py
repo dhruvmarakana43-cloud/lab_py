8775def09698f080cd6a8f9f15a67ada192a5cf4
Dhruv Marakana <dhruvmarakana43@gmail.com>
2026-02-23 11:26:33 +0530

    Add files via upload

---
+++ while.py
@@ -0,0 +1,21 @@
+#print number from 1 to 10
+i=1
+while i <=10:
+    print(i)
+    i=i+1
+    
+#sum of first n number
+n=int(input("enter no:"))
+i=1
+s=0
+while i <=10:
+    s=s+i
+    i=i+1
+    print("sum=",s)
+    
+#table of a number
+num=int(input("enter nm:"))
+i=1
+while i<=10:
+    print(num,"x",i,"=",num*i)
+    i=i+1
