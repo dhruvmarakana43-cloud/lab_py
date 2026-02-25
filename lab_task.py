a2edc62132acdb28f9ae70a6d98a74f291a479fd
Dhruv Marakana <dhruvmarakana43@gmail.com>
2026-02-25 09:08:02 +0530

    Add files via upload

---
+++ lab_task.py
@@ -0,0 +1,47 @@
+#1)
+i=1
+while i <=5:
+    print(i)
+    i=i+1
+#2)
+n=int(input("enter a number:"))
+i=1
+sum=0
+while i<=n:
+    sum=sum+i
+    i=i+1
+    print("sum is:",sum)
+
+#3)
+i=1
+while i<=20:
+    if i % 2 != 0:
+        print(i)
+    i=i+1
+    
+#4)
+i=1
+while i<=10:
+    print("4x",i,"=",4*i)
+    i=i+1
+    
+#5)
+
+i=10
+while i>=1:
+    print(i)
+    i=i-1
+    
+#6)
+num=[1,2,3,4,5,6,7,8,9]
+largest=max(num)
+print(largest)
+
+
+#7)
+i=1
+while i<=20:
+    if i % 2 == 0:
+        print(i)
+    i=i+1
+    
